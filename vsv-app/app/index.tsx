bd4f04de273c16bde0cc8ec749a8b6bbefd4bc0e
Sebma <147602507+seb-machac@users.noreply.github.com>
2025-07-02 23:36:47 +1000

    Not working

--- vsv-app/app/index.tsx
+++ vsv-app/app/index.tsx
@@ -1,25 +1,43 @@
-import { StyleSheet, Text, View} from "react-native";
+import React, { useState } from 'react';
+import MapView from 'react-native-maps';
+import * as Native_Map from 'react-native-maps';
+import { Alert, Button, Text, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+import {APIProvider, Map} from '@vis.gl/react-google-maps';
 
-export default function Index() {
+
+export default function App() {
+  // const api = process.env.EXPO_PUBLIC_API
+  async function onpress() {
+    console.log("started")
+    await Location.requestForegroundPermissionsAsync()
+    console.log(Location.PermissionStatus)
+    let location = await Location.getCurrentPositionAsync({});
+    let address = await Location.reverseGeocodeAsync(location.coords);
+    console.log(address);
+
+  }
   return (
-    <html>
-      <body>
-        <gmp-map center="-33.9,151.2" zoom="10" map-id="DEMO_MAP_ID">
-          
-        </gmp-map>
-      </body>
-    <Text style={style.text}>hello</Text>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAykSwMb92Ro1FKcBHZroNNETAOx7ZwYuQ&callback=console.debug&libraries=maps,marker&v=beta"
-      defer
-      ></script>
-    </html>
+    // <View style={styles.container}>
+    //   <Text>fe</Text>
+    //   <Button onPress={onpress} title={"test"}/>;
+    //   <MapView showsUserLocation={true} provider={Native_Map.PROVIDER_GOOGLE} style={styles.map}/>
+      <APIProvider apiKey={"AIzaSyAykSwMb92Ro1FKcBHZroNNETAOx7ZwYuQ"}>
+        <Map 
+        defaultCenter={{lat:20,lng:10}}
+        defaultZoom={3}/>
+      </APIProvider>
+    // {/* </View> */}
   );
 }
 
-
-const style = StyleSheet.create({
-  text: {
-    fontSize: 20,
-    color: "#3333",
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '25%',
   },
 });
+
