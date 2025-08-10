8387fa4020541f62edd9c711155070957070d93b
Sebma <147602507+seb-machac@users.noreply.github.com>
2025-08-10 17:06:38 +1000

    Gave Up on Vite, Got a real map with google api working, auto centers on current position.

---
+++ MapView/MapTestScript.js
@@ -0,0 +1,27 @@
+
+let map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 10,
+    disableDefaultUI: true,
+    mapId: "TestMap",
+  });
+  const locationButton = document.createElement("button");
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          map.setCenter(pos);
+        }
+      );
+    };
+  locationButton.click();
+};
+window.initMap = initMap;
