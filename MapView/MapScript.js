8cc0fff2d0d9fddfa914a7f874e2ef0d8a1dd509
Sebma <147602507+seb-machac@users.noreply.github.com>
2025-08-29 18:36:20 +1000

    Commenting

--- MapView/MapScript.js
+++ MapView/MapScript.js
@@ -1,99 +1,152 @@
 
+//Start Main async function for initialising 
 async function initMap() {
 
+    //Fetch Locations Data
     let response = await fetch('../Locations.json');
+    //Parse Locations Data
     const Locations = await response.json();
 
+    //Import Classes and Object Types from Maps Library
     const { Map, InfoWindow, RenderingType, setTilt } = await google.maps.importLibrary("maps");
+    //Import Classes and Object Types from Marker Library
     const { AdvancedMarkerElement, PinElement} = await google.maps.importLibrary("marker");
-
-
+    //Define New Map at Element "map"
     map = new Map(document.getElementById("map"), {
+        //Initial Center Position
         center: { lat: -37.814, lng: 144.96322 },
+        //Zoom level
         zoom: 10,
+        //Disable Maps UI
         disableDefaultUI: true,
+        //Set Rendering Type
         renderingType: RenderingType.RASTER,
+        //Hybrid Map Type
         mapTypeId: "hybrid",
+        //Map ID
         mapId: "TestMap",
     });
+    //Map Tilt Level
     map.setTilt(0);
 
-    
-    const textInput = document.getElementById('Search-Input');
-    const textInputButton = document.getElementById('Search-Button');
+    //Define Search Input
+    const SearchInput = document.getElementById('Search-Input');
+    //Define Search Button
+    const SearchInputButton = document.getElementById('Search-Button');
 
-    textInput.addEventListener('keydown', (event) => {
+    //Detect Keypress on Search Input
+    SearchInput.addEventListener('keydown', (event) => {
+        //True if Keypress was Enter:
         if (event.key === 'Enter') {
-            findPlaces(textInput.value);
+            //Call findPlaces with current Search Input value
+            findPlaces(SearchInput.value);
         }
     });
-    textInputButton.addEventListener('click', () => {
-        findPlaces(textInput.value);
+    //Detect Clicks on Search Button
+    SearchInputButton.addEventListener('click', () => {
+        //Call findPlaces with current Search Input value
+        findPlaces(SearchInput.value);
     });
+    //Create new InfoWindow
     infoWindow = new google.maps.InfoWindow();
-
+    
+    //Loop for Each Item in Location
     Locations.forEach((location) => {
+        //Create a new Map Marker
         new google.maps.marker.AdvancedMarkerElement({
+            //Apply to current map
             map: map,
+            //Set position to predefined location
             position: location,
+            //Set title to predefined title
             title: location.title,
+            //Customise Pin Image
             content: new PinElement({
+                        //Scale Pin
                         scale:2,
                     }).element,
         });
     });
 }
 
+//New async funtion to search places
 async function findPlaces(query) {
-  let markers = {};
-  const { Place } = await google.maps.importLibrary("places");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const request = {
-      textQuery: query,
-      fields: ['displayName', 'location', 'businessStatus'],
-      includedType: '', // Restrict query to a specific type (leave blank for any).
-      language: 'en-US',
-      maxResultCount: 100,
-      minRating: 1, // Specify a minimum rating.
-  };
-  const { places } = await Place.searchByText(request);
-  if (places.length) {
-      const { LatLngBounds } = await google.maps.importLibrary("core");
-      const bounds = new LatLngBounds();
-      // First remove all existing markers.
-      for (const id in markers) {
-          markers[id].map = null;
-      }
-      ;
-      markers = {};
-      // Loop through and get all the results.
-      places.forEach(place => {
-          const marker = new AdvancedMarkerElement({
-              map,
-              position: place.location,
-              title: place.displayName,
-          });
-          markers[place.id] = marker;
-          marker.addListener('gmp-click', () => {
-              map.panTo(place.location);
-              updateInfoWindow(place.displayName, place.id, marker);
-          });
-          if (place.location != null) {
-              bounds.extend(place.location);
-          }
-      });
-      map.fitBounds(bounds);
-  }
-  else {
-      console.log('No results');
-  }}
-  async function updateInfoWindow(title, content, anchor) {
-  infoWindow.setContent(content);
-  infoWindow.setHeaderContent(title);
-  infoWindow.open({
-      map,
-      anchor,
-      shouldFocus: false,
-  });
+    //Create Markers Array
+    let markers = {};
+    //Import Place from Library "places"
+    const { Place } = await google.maps.importLibrary("places");
+    //Import AdvancedMarkerElement from Library "marker"
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    //Create request Object with required values
+    const request = {
+        //Sets textQuery to function argument 
+        textQuery: query,
+        //Selects which fields are required
+        fields: ['displayName', 'location', 'businessStatus'],
+        //Request all Location Types
+        includedType: '',
+        //Sets Language
+        language: 'en-US',
+        //Sets Maximum Results to 100
+        maxResultCount: 100,
+        //Sets Minimum Rating to lowest possible for all locations
+        minRating: 1,
+    };
+    //Get all found Places from Request
+    const { places } = await Place.searchByText(request);
+    //True if any Places are found
+    if (places.length) {
+        //Import LatLngBounds from Library "core"
+        const { LatLngBounds } = await google.maps.importLibrary("core");
+        //Create New View Boundary
+        const bounds = new LatLngBounds();
+        //Loop for all Objects in Markers Array
+        for (const id in markers) {
+            //Remove Current Marker
+            markers[id].map = null;
+        };
+        //Redefine markers array
+        markers = {};
+        //Loop through each Found Place
+        places.forEach(place => {
+            //Create New Marker Element
+            const marker = new AdvancedMarkerElement({
+                //Apply to current Map
+                map,
+                //Set postition to current Place Location
+                position: place.location,
+                //Set Marker title to current Place Name
+                title: place.displayName,
+            });
+            //Define current Place ID for reference
+            markers[place.id] = marker;
+            //Detect Click on Marker
+            marker.addListener('gmp-click', () => {
+                //Move Map to Place Location
+                map.panTo(place.location);
+                //Display InfoWindow with Title, ID for Display, ID for Google reference
+                updateInfoWindow(place.displayName, place.id, marker);
+            });
+            //True if Place has Location Value
+            if (place.location != null) {
+                //Extend Boundary to Encompass Place Location
+                bounds.extend(place.location);
+            }
+        });
+        //Center Map to View Boundary
+        map.fitBounds(bounds);
+    }
+    //True if 
+    else {
+        window.CookieStore.set()("No Results")
+    }}
+    async function updateInfoWindow(title, content, anchor) {
+    infoWindow.setContent(content);
+    infoWindow.setHeaderContent(title);
+    infoWindow.open({
+        map,
+        anchor,
+        shouldFocus: false,
+    });
 }
 window.initMap = initMap;
